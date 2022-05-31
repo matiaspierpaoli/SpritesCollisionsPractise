c1ad64d43a693915e94de2c478b7e04f80124898
Matias <matiaspierpaoli@gmail.com>
2022-05-31 11:46:42 -0300

    Listo chequeo de bordes de pantalla

--- 24-05/Assets/Scripts/CollisionDetection.cs
+++ 24-05/Assets/Scripts/CollisionDetection.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollisionDetection : MonoBehaviour
 {
     private SpriteRenderer spr;
+    private Color originalColor;
     private float timer;
     private float maxSeconds;
 
@@ -13,21 +14,24 @@ public class CollisionDetection : MonoBehaviour
         timer = 0f;
         maxSeconds = 5;
         spr = GetComponent<SpriteRenderer>();
-       
+        originalColor = GetComponent<Color>();
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        for (int i = 0; i < maxSeconds; i++)
-            FlashAfterCollision();
-                
+    {               
         spr.color = Color.red;
-        
+       
+        FlashAfterCollision();
+
     }
 
     private void FlashAfterCollision()
     {
         timer += Time.deltaTime;
+        //float a = Mathf.Sin(timer) * 0.5 + 0.5;
+        spr.material.color = new Color(originalColor.r, originalColor.g, timer);
 
     }
 
--- 24-05/Assets/Scripts/SpriteMovement.cs
+++ 24-05/Assets/Scripts/SpriteMovement.cs
@@ -5,13 +5,51 @@ using UnityEngine;
 public class SpriteMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private SpriteRenderer spr;
+    Vector3 worldBounds;
+
+    private void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();       
+        worldBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    }
 
     private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        this.transform.Translate(moveHorizontal * speed * Time.deltaTime, moveVertical * speed * Time.deltaTime, 0.0f);     
+        this.transform.Translate(moveHorizontal * speed * Time.deltaTime, moveVertical * speed * Time.deltaTime, 0.0f);
+
+        ChechForBounds();
+    }
+
+    private void ChechForBounds()
+    {
+        Vector3 maxPosWorld = spr.bounds.max;
+        Vector3 maxPosViewPort = Camera.main.WorldToViewportPoint(maxPosWorld);
+
+        Vector3 minPosWorld = spr.bounds.min;
+        Vector3 minPosViewPort = Camera.main.WorldToViewportPoint(minPosWorld);
+
+        if (maxPosViewPort.x >= 1)
+            spr.transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+
+        if (minPosViewPort.x <= 0)
+            spr.transform.position = new Vector3(transform.position.x + worldBounds.x, transform.position.y, transform.position.z);
+
+        if (maxPosViewPort.y >= 1)
+            spr.transform.position = new Vector3(transform.position.x, transform.position.y - worldBounds.y, transform.position.z);
+        
+        if (minPosViewPort.y <= 0)
+            spr.transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+
+
     }
 
 }
